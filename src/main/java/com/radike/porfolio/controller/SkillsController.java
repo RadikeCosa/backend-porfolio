3484cdc7f4794d5b30ef740bc10ee0cb298def85
Ramiro Cosa <ramirocosa@gmail.com>
2022-11-01 10:20:21 -0300

    agregado cors y rest controller en todos los controller

--- src/main/java/com/radike/porfolio/controller/SkillsController.java
+++ src/main/java/com/radike/porfolio/controller/SkillsController.java
@@ -1,4 +1,10 @@
 package com.radike.porfolio.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class SkillsController {
 }

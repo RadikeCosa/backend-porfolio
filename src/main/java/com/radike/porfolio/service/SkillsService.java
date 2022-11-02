66bdf39413c05f7564aa454adb85ca94d809a60a
Ramiro Cosa <ramirocosa@gmail.com>
2022-11-02 18:54:59 -0300

    skills terminado

--- src/main/java/com/radike/porfolio/service/SkillsService.java
+++ src/main/java/com/radike/porfolio/service/SkillsService.java
@@ -1,4 +1,35 @@
 package com.radike.porfolio.service;
 
-public class SkillsService {
+import com.radike.porfolio.model.Skills;
+import com.radike.porfolio.repository.SkillsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillsService implements ISkillsService{
+
+    @Autowired
+    private SkillsRepository skillsRepository;
+
+    @Override
+    public List<Skills> getSkills() {
+        return skillsRepository.findAll();
+    }
+
+    @Override
+    public void saveSkills(Skills skills) {
+        skillsRepository.save(skills);
+    }
+
+    @Override
+    public void deleteSkills(Long id) {
+        skillsRepository.deleteById(id);
+    }
+
+    @Override
+    public Skills findSkills(Long id) {
+        return skillsRepository.findById(id).orElse(null);
+    }
 }
